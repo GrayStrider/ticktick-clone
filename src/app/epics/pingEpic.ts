e92974e53828639e05033c76f235d98409c34452
Ivan <zhoga.ivan@gmail.com>
2019-05-13 07:58:59 +1100

    before hotreload epic

--- src/app/epics/pingEpic.ts
+++ src/app/epics/pingEpic.ts
@@ -1,10 +1,13 @@
-import { delay, filter, mapTo } from 'rxjs/operators';
-import { Action, getType, isActionOf } from 'typesafe-actions';
+import { delay, filter, mapTo, tap } from 'rxjs/operators';
+import { action, Action, getType, isActionOf } from 'typesafe-actions';
 import { ping, pong } from 'app/actions/pingPong';
 import { Epic } from 'redux-observable';
 
 export const pingEpic: Epic<Action<any>, Action<any>, void, any> = (action$, state$) => action$.pipe(
   filter(isActionOf(ping)),
   delay(1000),
-  mapTo({type: getType(pong)})
+  mapTo({type: getType(pong)}),
+  tap(action$ => console.log('tapped: ' + action$)),
+  delay(1000),
+  mapTo({type: 'TEST'})
 );
