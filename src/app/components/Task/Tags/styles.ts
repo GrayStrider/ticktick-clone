8f43aa63ddf51914191c7d3f6513c30129dc6be2
GrayStrider <zhoga.ivan@gmail.com>
2019-05-15 10:08:02 +1100

    padding margin

--- src/app/components/Task/Tags/styles.ts
+++ src/app/components/Task/Tags/styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+  display: flex;
+  //background: red;
+  padding: 0.3em;
+  align-items: center;
   .label {
     padding: 0.3em 0.3em 0.1em 0.3em !important;
     border-radius: 1px !important;
