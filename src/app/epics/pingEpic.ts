bf4687142679bab03da2f7abe8b9f5788db9dffb
Ivan <zhoga.ivan@gmail.com>
2019-05-14 18:40:38 +1100

    rxjs experiments

--- src/app/epics/pingEpic.ts
+++ src/app/epics/pingEpic.ts
@@ -1,14 +1,33 @@
-import {delay, filter, flatMap, mapTo, tap } from 'rxjs/operators';
-import {concat} from 'rxjs';
-import { action, Action, getType, isActionOf } from 'typesafe-actions';
+import { delay, flatMap, mapTo, repeat, tap } from 'rxjs/operators';
+import { concat } from 'rxjs';
+import { Action, getType } from 'typesafe-actions';
 import { ping, pong } from 'app/actions/pingPong';
 import { Epic, ofType } from 'redux-observable';
-import { Observable, of } from 'rxjs';
+import { of } from 'rxjs';
 
-export const pingEpic: Epic<Action<any>, Action<any>, void, any> = (action$, state$) => action$.pipe(
+// export const pingEpic: Epic<Action<any>, Action<any>, void, any> = (action$, state$) => action$.pipe(
+//   ofType(getType(ping)),
+//   flatMap(action => concat(
+//     of({type: getType(pong)}),
+//     of({type: 'TEST'}).pipe(delay(5000))
+//   ))
+// );
+
+/**
+ * 1) log before wait with tap
+ * 2) pipe the pipe for "repeat"
+ *
+ */
+
+export const pingEpic2: Epic<Action<any>, Action<any>, void, any> = (action$, state$) => action$.pipe(
   ofType(getType(ping)),
   flatMap(action => concat(
-    of({type: getType(pong)}),
-    of({type: 'TEST'}).pipe(delay(5000))
-  ))
+    of({ type: 'TEST2' }).pipe(
+      tap(() => console.log('before emition')),
+      delay(2000))
+      .pipe(repeat(3)
+      )
+    )
+  )
 );
+
